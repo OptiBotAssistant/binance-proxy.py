745f2c8d6b04e114792637c763a9c0fa753211da
OptiBotAssistant <optibot.app@gmail.com>
2025-05-27 02:45:28 +0300

    binance-proxy.py

--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -20,3 +21,8 @@ def proxy_binance():
 @app.route("/")
 def home():
     return "Binance Proxy is running"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
